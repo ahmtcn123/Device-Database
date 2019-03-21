7f29bf1acba396a815646ce9b82f8605f4f24def
Ahmetcan Aksu <ahmetcanco@gmail.com>
2019-03-21 20:59:40 +0300

    v1.1.3
    Update for new proc/cpuinfo

--- index.js
+++ index.js
@@ -1,52 +1,67 @@
+// Copyright 2019, Project Ann: Ahmetcan Aksu
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//    http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
 /** 
  * Database constructor
-*/
-exports.data = function() {
+ */
+exports.data = function () {
 	var query = "";
 	try {
 		query = require("./lib/module").rev("/proc/cpuinfo");
-	} catch(err) {
-		var er = new Error("Cannot parse device data. Data is corrupted")
-		throw er
-	}
-	var db    =  require("./lib/src/database.json");
-	var devicePropeties = query.find(e => e.revision !== undefined)
+	} catch (err) {
+		var er = new Error("Cannot parse device data. Data is corrupted");
+		er.code = "brokenData";
+		throw er;
+	};
+	var db = require("./res/database.json");
+	var devicePropeties = query.find(e => e.revision !== undefined);
 	var found = "";
 	try {
 		found = db[devicePropeties.revision];
-	} catch(err) {
-		var er = new Error("Cannot parse device data. Data is corrupted")
-		throw er
-	}		
+	} catch (err) {
+		var er = new Error("Cannot parse device data. Data is corrupted");
+		er.code = "brokenData";
+		throw er;
+	};
 	/**
 	 * Get device properties
 	 * @returns {object} object - Get parsed device data
 	 */
-	this.getDevice = function() {
+	this.getDevice = function () {
 		return query.find(e => e.revision !== undefined);
-	}
+	};
 
 	/**
 	 * Get parsed data
 	 * @returns {object} object - All parsed data
 	 */
-	this.getParsed = function() {
+	this.getParsed = function () {
 		return query;
-	}
+	};
 
 	/**
 	 * Get device properties from database
 	 * @returns {object} object - Database result
-	 */	
-	this.getResult = function() {
-		return found == undefined ? null : found;
-	}
+	 */
+	this.getResult = function () {
+		return found !== undefined;
+	};
 
 	/**
 	 * Get device result boolean
 	 * @returns {boolean} - Boolean
-	 */	
-	this.found = function() {
-		return found == undefined ? false : true;
-	}
-}
+	 */
+	this.found = function () {
+		return found !== undefined;
+	};
+};
